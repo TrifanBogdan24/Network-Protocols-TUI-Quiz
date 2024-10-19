abb1323b3bdc0650c016b8e726ceeacd7b400266
TrifanBogdan24 <trifanbogdan24@gmail.com>
2024-10-19 15:37:58 +0300

    Initial commit: importing libraries

---
+++ index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { intro, note, outro, select, spinner, text } from '@clack/prompts';
+import { setTimeout } from 'node:timers/promises';
+import color from 'picocolors';
+
